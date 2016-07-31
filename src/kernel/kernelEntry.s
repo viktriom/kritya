bcdf75fd5ba1861afcf9017f909c9ffe1ec9e90d
Vivek Tripathi <viktriom@gmail.com>
2016-07-31 07:20:30 +0530

    remove file

--- src/kernel/kernelEntry.s
+++
@@ -1,9 +0,0 @@
-;exsures that we jump straight into kernel's entry function
-
-[bits 32]           ;we are in the protected mode
-[extern main]       ;Declare that we will be refrencing an external symbol 'main' 
-                    ;So inliner can resolve it duing linking.
-
-call main           ;invoke the 'main' function in our c code.
-
-jmp $              ;hang for ever when we return from kernal
